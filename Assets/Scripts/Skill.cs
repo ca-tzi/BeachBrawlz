d6655592ce5e3293d578b5fb56a42b1c6aebead8
ca-tzi <ectoderms@gmail.com>
2021-05-29 14:55:37 -0400

    Update Skill.cs

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Skill : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public string skillName;
+  public string skillDescription;
+  public int atkValue;
+  public int accValue;
 }
